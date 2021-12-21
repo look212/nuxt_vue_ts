074328be8d0667d68059fae84cdc49c2fc7f3c12
조보화 <jbh@x2soft.co.kr>
2021-12-21 15:26:56 +0900

    unplugin-vue2-script-setup 패키지 설치 후 build 재시도

---
+++ docs/_nuxt/4f91006.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{336:function(e,n,t){"use strict";t.r(n);var c={name:"index"},r=t(74),component=Object(r.a)(c,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("admin")])}),[],!1,null,"447e0892",null);n.default=component.exports}}]);
